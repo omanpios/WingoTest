32ce7ba265b967fb882337d82e3fca987e7551ff
Omar <omanpios88@gmail.com>
2021-02-03 07:29:16 -0500

    Resources folder

---
+++ src/main/java/resources/ExtentReporterNG.java
@@ -0,0 +1,32 @@
+package resources;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentReporterNG {
+
+	static ExtentReports extent;
+
+	public static ExtentReports getReportObject() {
+
+		Calendar calendar = Calendar.getInstance();
+		SimpleDateFormat formater = new SimpleDateFormat("ddMMyyy_hhmmss");
+
+		String path = System.getProperty("user.dir") + "\\reports\\index" + formater.format(calendar.getTime())
+				+ ".html";
+		ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+		reporter.config().setReportName("Wingo Automation Results");
+		reporter.config().setDocumentTitle("Test Results");
+
+		extent = new ExtentReports();
+		extent.attachReporter(reporter);
+		extent.setSystemInfo("Tester", "Omar Pinilla");
+		
+
+		return extent;
+
+	}
+}
